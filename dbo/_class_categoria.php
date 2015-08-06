2bdadbde240776c87f6374afbb266d6c02ee5ff4
Jose Eduardo Biasioli <biasiolije@gmail.com>
2015-08-06 09:47:20 -0300

    adicionadas classes meta e categoria

---
+++ dbo/_class_categoria.php
@@ -0,0 +1,337 @@
+<?
+
+/* ================================================================================================================== */
+/* DBO CLASS FILE FOR MODULE 'categoria' ======================================== AUTO-CREATED ON 20/07/2015 13:52:00 */
+/* ================================================================================================================== */
+
+/* IMPORTANT: This file is generated only in the first DBO sync, what means you should edit only via text editor. */
+
+if(!class_exists('categoria'))
+{
+	class categoria extends dbo
+	{
+		/* smart constructor: will perform load() upon numeric argument and loadAll() upon string argument */
+		function __construct($foo = '')
+		{
+			parent::__construct('categoria');
+			if($foo != '')
+			{
+				if(is_numeric($foo))
+				{
+					$this->id = $foo;
+					$this->load();
+				}
+				elseif(is_string($foo))
+				{
+					$this->loadAll($foo);
+				}
+			}
+		}
+
+		//sobrecarga
+		function save()
+		{
+			$id = parent::save();
+			if(is_numeric($this->mae))
+			{
+				//tira a flag de folha do pai, se for o caso.
+				$cat = new categoria($this->mae);
+				if($cat->folha == 1)
+				{
+					$cat->folha = 0;
+					$cat->update();
+				}
+			}
+			return $id;
+		}
+
+		static function getCategoryStructure($pagina_tipo = null, $params = array())
+		{
+			global $_system;
+			extract($params);
+
+			$tree = array();
+
+			$cat = new categoria();
+			$sql = "
+				SELECT * FROM ".$cat->getTable()." 
+				WHERE 
+					mae ".($mae ? " = '".$mae."' " : " IS NULL AND 
+					pagina_tipo = '".$pagina_tipo."' ")." 
+				ORDER BY nome
+			";
+			$cat->query($sql);
+			if($cat->size())
+			{
+				do {
+					$dados = array(
+						'id' => $cat->id,
+						'slug' => $cat->slug,
+						'nome' => $cat->nome,
+						'full_slug' => $_system['pagina_tipo'][$pagina_tipo]['slug_prefix'].'/categorias/'.$cat->slug,
+					);
+					if($cat->folha == 0)
+					{
+						$params['full_slug_prefix'] = $_system['pagina_tipo'][$pagina_tipo]['slug_prefix'].'/categorias/'.$cat->slug.'/';
+						$dados['children'] = categoria::getCategoryChildren($cat, $params);
+					}
+					$tree[] = $dados;
+				}while($cat->fetch());
+			}
+			return $tree;
+		}
+
+		static function getCategoryChildren($cat, $params = array())
+		{
+			extract($params);
+
+			$tree = array();
+
+			$filha = new categoria();
+			$sql = "
+				SELECT * FROM ".$cat->getTable()." 
+				WHERE 
+					mae = '".$cat->id."'
+				ORDER BY nome;
+			";
+			$filha = new categoria();
+			$filha->query($sql);
+			if($filha->size())
+			{
+				do {
+					$dados = array(
+						'id' => $filha->id,
+						'slug' => $filha->slug,
+						'nome' => $filha->nome,
+						'full_slug' => $full_slug_prefix.$filha->slug,
+					);
+					if($filha->folha == 0)
+					{
+						$params['full_slug_prefix'] = $full_slug_prefix.$filha->slug.'/';
+						$dados['children'] = categoria::getCategoryChildren($filha, $params);
+					}
+					$tree[] = $dados;
+				}while($filha->fetch());
+			}
+			return $tree;
+		}
+
+		static function startCategoriaEngine()
+		{
+			if(thisPage().".php" == CATEGORIA_ENGINE_FILE)
+			{
+				categoria::categoriaEngine();
+			}
+		}
+		
+		static function categoriaEngine()
+		{
+			if(thisPage().".php" == CATEGORIA_ENGINE_FILE)
+			{
+				global $_system;
+				global $_pagina;
+				global $_pagina_backup;
+				global $_category_tree;
+				global $_pagina_tipo;
+				global $_categoria;
+				//situação para categorias especificas
+				if($_GET['slug'])
+				{
+					$slug = preg_replace('/\/$/is', '', $_GET['slug']);
+					$slug = explode("/", $slug);
+					$slug = array_pop($slug);
+					$_categoria = new categoria("WHERE slug = '".dboescape($slug)."'");
+					if($_categoria->size())
+					{
+						$_pagina = new pagina();
+						$_pagina_tipo = $_categoria->pagina_tipo;
+						$_category_tree[$_categoria->pagina_tipo] = categoria::getCategoryStructure($_pagina_tipo);
+						queryPaginas(array(
+							'cat' => $_categoria->id,
+							'tipo' => $_pagina_tipo,
+						));
+						//testa especificidade pelas cutomizações do tempalte
+						if(file_exists($_pagina_tipo.'-categoria-'.$_categoria->slug.'.php'))
+						{
+							include($_pagina_tipo.'-categoria-'.$_categoria->slug.'.php');
+							exit();
+						}
+						elseif(file_exists($_pagina_tipo.'-categoria.php'))
+						{
+							include($_pagina_tipo.'-categoria.php');
+							exit();
+						}
+					}
+					//se não existe a página requisitada no banco, quer dizer que está tentando acessar algum outro arquivo. Então redirecionamos para ele.
+					elseif(file_exists($_GET['slug']))
+					{
+						//isso dá loop infinito... consertar depois
+						header("Location: ".SITE_URL."/".$_GET['slug']);
+						exit();
+					}
+					elseif(file_exists('404.php'))
+					{
+						include('404.php');
+						exit();
+					}
+					else
+					{
+						echo "404";
+						exit();
+					}
+				} 
+				elseif($_GET['slug_prefix'])
+				{
+					foreach($_system['pagina_tipo'] as $tipo => $data)
+					{
+						if($data['slug_prefix'] == $_GET['slug_prefix'])
+						{
+							$_pagina = new pagina();
+							$_pagina_tipo = $tipo;
+							queryPaginas(array(
+								'tipo' => $tipo,
+							));
+							$_category_tree[$tipo] = categoria::getCategoryStructure($tipo);
+
+							//testa especificidade pelas cutomizações do tempalte
+							if(file_exists($_pagina_tipo.'-categorias.php'))
+							{
+								include($_pagina_tipo.'-categorias.php');
+								exit();
+							}
+							elseif(file_exists($_pagina_tipo.'-categoria.php'))
+							{
+								include($_pagina_tipo.'-categoria.php');
+								exit();
+							}
+						}
+					}
+				}
+			}
+		}			
+
+		static function getCategoriaInfo($cat_id, $tree, $params = array())
+		{
+			extract($params);
+			/*
+				@params
+				 key: id | slug
+			*/
+
+			//valores default
+			$key = $key !== null ? $key : 'id';
+
+			//busca recursiva pelas informações deste id
+			if(sizeof($tree))
+			{
+				foreach($tree as $node)
+				{
+					if($node[$key] == $cat_id)
+					{
+						//se for para retornar somente folhas, mas este tiver filhos, não retorna.
+						if($somente_folhas && is_array($node['children']))
+						{
+							return false;
+						}
+						return $node;
+					}
+					elseif(is_array($node['children']))
+					{
+						$return = categoria::getCategoriaInfo($cat_id, $node['children'], $params);
+					}
+
+					if($return)
+					{
+						return $return;
+					}
+				}
+			}
+		}
+
+		static function renderTreeMenu($tree, $params = array())
+		{
+			extract($params);
+			if(sizeof($tree))
+			{
+				ob_start();
+				?>
+				<ul class="<?= $classes ?>">
+					<?php
+						foreach($tree as $node)
+						{
+							?>
+							<li><a href="<?= SITE_URL ?>/<?= $node['full_slug'] ?>"><?= $node['nome'] ?></a>
+							<?php
+							if(is_array($node['children']))
+							{
+								$params['classes'] .= ' children ';
+								echo categoria::renderTreeMenu($node['children'], $params);
+							}
+							?>
+							</li>
+							<?php
+						}
+					?>
+				</ul>
+				<?php
+				return ob_get_clean();
+			}
+		}
+	} //class declaration
+} //if ! class exists
+
+//definindo o nome padrão para o arquivo de processamento de página
+define(CATEGORIA_ENGINE_FILE, 'categoria.php');
+
+//hook para criação categorias dinâmicas
+global $hooks;
+$hooks->add_action('dbo_includes_after', 'categoria::startCategoriaEngine');
+
+//funções para templating
+
+function carregaArvoreDeCategorias($pagina_tipo = null, $params = array())
+{
+	global $_category_tree;
+	$_category_tree[$pagina_tipo] = categoria::getCategoryStructure($pagina_tipo, $params);
+}
+
+function arvoreDeCategorias($pagina_tipo = null)
+{
+	global $_category_tree;
+	if($pagina_tipo)
+	{
+		return $_category_tree[$pagina_tipo];
+	}
+	return $_category_tree;
+}
+
+function categoriaNome()
+{
+	global $_categoria;
+	return $_categoria->nome;
+}
+
+function haSubcategorias()
+{
+	global $_categoria;
+	global $_category_tree;
+	global $_pagina_tipo;
+	$foo = categoria::getCategoriaInfo($_categoria->id, $_category_tree[$_pagina_tipo]);
+	return is_array($foo['children']);
+}
+
+function subcategorias($separator, $params = array())
+{
+	global $_categoria;
+	global $_category_tree;
+	global $_pagina_tipo;
+	$cats = categoria::getCategoriaInfo($_categoria->id, $_category_tree[$_pagina_tipo]);
+	$cats = $cats['children'];
+	foreach($cats as $cat)
+	{
+		$return .= '<a href="'.$cat['full_slug'].'">'.$cat['nome'].'</a>'.(end($cats) != $cat ? $separator : '');
+	}
+	return $return;
+}
+
+?>
---
+++ dbo/_dbo_categoria.php
@@ -0,0 +1,299 @@
+<?
+
+/* ================================================================================================================== */
+/* DBO DEFINITION FILE FOR MODULE 'categoria' =================================== AUTO-CREATED ON 24/07/2015 11:57:42 */
+/* ================================================================================================================== */
+
+
+
+/* GENERAL MODULE DEFINITIONS ======================================================================================= */
+
+$module = new Obj();
+$module->modulo = 'categoria';
+$module->tabela = 'categoria';
+$module->titulo = 'Categoria';
+$module->titulo_plural = 'Categorias';
+$module->genero = 'a';
+$module->paginacao = '20';
+$module->update = true;
+$module->delete = true;
+$module->insert = 'Nova Categoria';
+$module->preload_insert_form = true;
+$module->auto_view = true;
+$module->order_by = '14';
+
+/* FIELDS =========================================================================================================== */
+
+$field = new Obj();
+$field->titulo = 'Id';
+$field->coluna = 'id';
+$field->pk = true;
+$field->isnull = false;
+$field->add = false;
+$field->valida = false;
+$field->edit = false;
+$field->view = false;
+$field->lista = false;
+$field->filter = false;
+$field->order = false;
+$field->type = 'INT NOT NULL auto_increment';
+$field->interaction = '';
+$field->tipo = 'pk';
+$module->campo[$field->coluna] = $field;
+
+/*==========================================*/
+
+$field = new Obj();
+$field->titulo = 'Nome';
+$field->coluna = 'nome';
+$field->pk = false;
+$field->isnull = false;
+$field->add = false;
+$field->valida = false;
+$field->edit = false;
+$field->view = false;
+$field->lista = false;
+$field->filter = false;
+$field->order = false;
+$field->type = 'VARCHAR(255)';
+$field->interaction = '';
+$field->tipo = 'text';
+$module->campo[$field->coluna] = $field;
+
+/*==========================================*/
+
+$field = new Obj();
+$field->titulo = 'Slug';
+$field->coluna = 'slug';
+$field->pk = false;
+$field->isnull = false;
+$field->add = false;
+$field->valida = false;
+$field->edit = false;
+$field->view = false;
+$field->lista = false;
+$field->filter = false;
+$field->order = false;
+$field->type = 'VARCHAR(255)';
+$field->interaction = '';
+$field->tipo = 'text';
+$module->campo[$field->coluna] = $field;
+
+/*==========================================*/
+
+$field = new Obj();
+$field->titulo = 'Descrição';
+$field->coluna = 'descricao';
+$field->pk = false;
+$field->isnull = false;
+$field->add = false;
+$field->valida = false;
+$field->edit = false;
+$field->view = false;
+$field->lista = false;
+$field->filter = false;
+$field->order = false;
+$field->type = 'TEXT';
+$field->interaction = '';
+$field->tipo = 'textarea';
+$module->campo[$field->coluna] = $field;
+
+/*==========================================*/
+
+$field = new Obj();
+$field->titulo = 'Mãe';
+$field->coluna = 'mae';
+$field->pk = false;
+$field->isnull = true;
+$field->add = false;
+$field->valida = false;
+$field->edit = false;
+$field->view = false;
+$field->lista = false;
+$field->filter = false;
+$field->order = false;
+$field->type = 'INT(11)';
+$field->interaction = '';
+$field->tipo = 'join';
+	$join = new Obj();
+	$join->modulo = 'categoria';
+	$join->chave = 'id';
+	$join->valor = 'nome';
+	$join->tipo = 'select';
+	$join->order_by = 'nome';
+$field->join = $join;
+$module->campo[$field->coluna] = $field;
+
+/*==========================================*/
+
+$field = new Obj();
+$field->titulo = 'Folha';
+$field->coluna = 'folha';
+$field->pk = false;
+$field->isnull = false;
+$field->add = false;
+$field->valida = false;
+$field->edit = false;
+$field->view = false;
+$field->lista = false;
+$field->filter = false;
+$field->order = false;
+$field->type = 'INT';
+$field->interaction = '';
+$field->tipo = 'text';
+$module->campo[$field->coluna] = $field;
+
+/*==========================================*/
+
+$field = new Obj();
+$field->titulo = 'Tipo de página';
+$field->coluna = 'pagina_tipo';
+$field->pk = false;
+$field->isnull = false;
+$field->add = false;
+$field->valida = false;
+$field->edit = false;
+$field->view = false;
+$field->lista = false;
+$field->filter = false;
+$field->order = false;
+$field->type = 'VARCHAR(255)';
+$field->interaction = '';
+$field->tipo = 'text';
+$module->campo[$field->coluna] = $field;
+
+/*==========================================*/
+
+/* GRID FOR THE FORM LAYOUT ========================================================================================= */
+
+$grid = array();
+
+
+$module->grid = $grid;
+
+/* MODULE LIST BUTTONS ============================================================================================== */
+
+/* FUNÇÕES AUXILIARES =============================================================================================== */
+
+if(!function_exists('categoria_pre_insert'))
+{
+	function categoria_pre_insert () // there is nothing to get as a parameter, right?
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_pos_insert'))
+{
+	function categoria_pos_insert ($obj) // active just inserted object
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_pre_update'))
+{
+	function categoria_pre_update ($obj) // active object
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_pos_update'))
+{
+	function categoria_pos_update ($obj) // active updated object
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_pre_delete'))
+{
+	function categoria_pre_delete ($obj) // active object
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_pos_delete'))
+{
+	function categoria_pos_delete ($obj) // active deleted object
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_pre_list'))
+{
+	function categoria_pre_list () // nothing to be passed here...
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_pos_list'))
+{
+	function categoria_pos_list ($ids) // ids of the listed elements
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_notifications'))
+{
+	function categoria_notifications ($type = '')
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+if(!function_exists('categoria_overview'))
+{
+	function categoria_overview ($foo)
+	{ global $dbo;
+	// ----------------------------------------------------------------------------------------------------------
+
+
+
+	// ----------------------------------------------------------------------------------------------------------
+	}
+}
+
+
+?>
